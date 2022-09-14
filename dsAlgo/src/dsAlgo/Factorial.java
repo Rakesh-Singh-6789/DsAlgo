b7b530b6de26c0693ab571cd65b9ba0df798fac6
csuftitan <csuftitan@LTMacAiM1165362.lan>
2022-09-13 22:06:39 -0700

    fact

---
+++ dsAlgo/src/dsAlgo/Factorial.java
@@ -0,0 +1,27 @@
+package dsAlgo;
+import java.io.*;
+
+public class Factorial {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		
+		System.out.print(factorial(num));
+	}
+
+	private static int factorial(int num) {
+		// TODO Auto-generated method stub
+		if(num == 1 || num == 0) {
+			return 1;
+		}
+		
+		int fact = num*factorial(num-1);
+		return fact;
+	}
+	
+	
+
+}
