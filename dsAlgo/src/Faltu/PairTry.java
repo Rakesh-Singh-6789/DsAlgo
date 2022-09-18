f402526c6067c9243ce4b87ae07a4c9c75daebdb
csuftitan <csuftitan@LTMacAir-M1-165362.local>
2022-09-18 15:17:57 -0700

    add saved pair class

--- dsAlgo/src/Faltu/PairTry.java
+++ dsAlgo/src/Faltu/PairTry.java
@@ -5,22 +5,25 @@ import java.util.*;
 import java.lang.*;
 
 public class PairTry {
+	
+	
 	public static class Pair{
-        int u,v;
-        public Pair(int u, int v){
-            this.u = u;
-            this.v = v;
-        } 
-    }
-    public static class Sorting implements Comparator<Pair>{
-        public int compare(Pair p1, Pair p2){
-            if(p2.v==p1.v){
-                return p2.u-p1.u;
-            }
-            return p1.u - p2.u;
-        }
-    }
-    
+	    int u,v;
+	    public Pair(int u, int v){
+	        this.u = u;
+	        this.v = v;
+	    } 
+	}
+	public static class Sorting implements Comparator<Pair>{
+	    public int compare(Pair p1, Pair p2){
+	        if(p2.v==p1.v){
+	            return p2.u-p1.u;
+	        }
+	        return p1.u - p2.u;
+	    }
+	}
+	
+	
     public static void main	(String[] args) {
     	int arr1[] = new int[] {2,5,23,7,3,6,8,1,347,0};
     	int arr2[] = new int[] {1,2,3,4,5,6,7,8,9,10};
@@ -41,4 +44,5 @@ public class PairTry {
     	}
     }
 }
+
 
