382e93a0a2fe9ff5a367574473471349cd815c34
Rakesh Singh <rakeshsr6789@gmail.com>
2022-10-14 14:16:20 -0700

    print encoding

--- dsAlgo/src/Recursion/encoding.java
+++ dsAlgo/src/Recursion/encoding.java
@@ -5,32 +5,29 @@ import java.util.*;
 
 public class encoding {
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int number = Integer.parseInt(br.readLine());
-		String str = String.valueOf(number);
+		// int number = Integer.parseInt(br.readLine());
+		String str = br.readLine();
 		// String str1 = number+"";
-		printEncodes(str, "");
+		printEncodings(str, "");
 
 	}
-	
-	private static void printEncodes(String str, String ans) {
-		// TODO Auto-generated method stub
-		
-		if(str.length() == 0) {
+
+	public static void printEncodings(String str, String ans) {
+		if (str.length() == 0) {
 			System.out.println(ans);
 			return;
 		}
-		
+
 		for (int i = 1; i <= str.length(); i++) {
-			int num = Integer.parseInt(str.substring(0,i));
-			if(num>26) {
+			long num = Long.parseLong(str.substring(0, i));
+			if (num > 26 || num < 1) {
 				continue;
 			}
-			char ch = (char) (num+96);
+			char ch = (char) (num + 96);
 			String ros = str.substring(i);
-			printEncodes(ros,ans+ch);
+			printEncodings(ros, ans + ch);
 		}
 	}
 
