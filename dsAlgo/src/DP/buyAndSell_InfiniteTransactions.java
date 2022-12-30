7b832227636f171b7a0322117add07dfe3fc1123
Rakesh Singh <rakeshsingh@Rakeshs-MBP.lan>
2022-12-29 19:45:39 -0800

    buy&sell stock infinite transactions

---
+++ dsAlgo/src/DP/buyAndSell_InfiniteTransactions.java
@@ -0,0 +1,38 @@
+package DP;
+
+import java.io.*;
+import java.util.*;
+
+public class buyAndSell_InfiniteTransactions {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int prices[] = new int[n];
+				
+		for(int i = 0; i<n; i++) {
+			prices[i] = Integer.parseInt(br.readLine());
+		}
+		
+		long overAllProfit = 0;
+		int buy = prices[0];
+		int sell = prices[0];
+		
+		for(int i =1; i<n ; i++) {
+			
+			if(prices[i] < sell) {
+				overAllProfit += (sell - buy);
+				buy = sell = prices[i];
+			} else {
+				sell = prices[i];
+			}
+			
+			
+		}
+		System.out.println(overAllProfit);
+	}
+
+}
